af53bfe0ed1fa9cefa97f62d3c15666085ec127b
Shaina Shilpi <98258789+ShainaShilpi@users.noreply.github.com>
2023-12-08 21:15:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Transform to prime/transform-to-prime.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcases = Integer.parseInt(br.readLine());
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(a1[i]);
+            }
+            Solution ob = new Solution();
+            int ans=ob.minNumber(a,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int minNumber(int arr[], int N)
+    {
+        
+        int sum = 0;
+        for(int i:arr){
+            sum += i;
+        }
+        if(isPrime(sum)) return 0;
+        for(int i = 1; i <= 100; i++){
+            sum += i;
+            if(isPrime(sum)) return i;
+            
+             sum -= i;
+        }
+        return 0;
+    }
+    
+    private boolean isPrime(int n){
+        if(n == 2) return true;
+        if(n == 1) return false;
+        
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+        
+    }
+}
