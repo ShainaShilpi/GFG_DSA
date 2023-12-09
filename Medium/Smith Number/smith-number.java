e56565a6a055221b1e6072e084c7ee94a98c17c6
Shaina Shilpi <98258789+ShainaShilpi@users.noreply.github.com>
2023-12-09 13:22:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smith Number/smith-number.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.smithNum(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int smithNum(int n) {
+        // code here
+        
+        int i,j;
+        int s = 0;
+        int c = n;
+        int sm = 0, sc;
+        
+        s = sum(n);
+        int d = n / 2;
+        
+        for(i = 2; i <= d; i++)
+        {
+            c = n;
+            if(prime(i) == 1 && c % i == 0) {
+                
+                sc = sum(i);
+                while(c % i == 0)
+                {
+                    sm = sm + sc;
+                    c /= i;
+                }
+            }
+        }
+        
+        if(s == sm)
+            return 1;
+        return 0;
+    }
+    
+    static int sum(int c) {
+        
+        int s = 0;
+        while(c != 0)
+        {
+            s = s + (c % 10);
+            c /= 10;
+        }
+        
+        return s;
+    }
+    
+    static int prime(int n) {
+        int m = (int)Math.sqrt(n);
+        int i;
+        for(i = 2; i <= m; i++) {
+            
+            if(n % i == 0)
+                return 0;
+        }
+        return 1;
+        
+    }
+};
